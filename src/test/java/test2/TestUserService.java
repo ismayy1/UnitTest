2fddfbda668fd6e7c52ad700b639794390c56043
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-01-30 16:39:43 +0200

    JUnit Test

--- src/test/java/test2/TestUserService.java
+++ src/test/java/test2/TestUserService.java
@@ -1,6 +1,12 @@
 package test2;
 
+import com.tpe.mockito2.UserRepository;
+import com.tpe.mockito2.UserService;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class TestUserService {
 
@@ -12,5 +18,19 @@ public class TestUserService {
     @Test
     void testGetUser() {
 
+        UserRepository userRepository = mock(UserRepository.class);
+
+        when(userRepository.getUserName(1)).thenReturn("bobSmith");
+        when(userRepository.getUserName(99)).thenReturn(null);
+
+        UserService userService = new UserService(userRepository);
+
+        String userName = userService.getUser(1);
+
+        assertEquals("Dear bobSmith", userName);
+//        verify(userRepository).getUserName(1);
+
+        assertThrows(RuntimeException.class, () -> userService.getUser(99));
+        verify(userRepository).getUserName(99);
     }
 }
