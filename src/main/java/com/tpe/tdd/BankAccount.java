0db946cf7842d44170ffdaed39616a16036e2fce
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-01-31 13:48:29 +0200

    JUnit Test

--- src/main/java/com/tpe/tdd/BankAccount.java
+++ src/main/java/com/tpe/tdd/BankAccount.java
@@ -11,7 +11,7 @@ public class BankAccount {
 
     private Double dailyWithdrawalLimit;
 
-    private Double dailyWithdrawalAmount;
+    private Double dailyWithdrawnAmount = 0.0;
 
     private List<String> history = new ArrayList<>();
 
@@ -32,5 +32,31 @@ public class BankAccount {
 
     public Double getDailyWithdrawalLimit() {
         return this.dailyWithdrawalLimit;
+    }
+
+    public List<String> getHistory() {
+        return this.history;
+    }
+
+//    Functional methods
+    public void deposit(Double amount) {
+
+        if (amount > 0) {
+            this.balance += amount;
+            this.history.add("Deposit: " + amount);
+        } else {
+            throw new RuntimeException("Amount must be positive.");
+        }
+    }
+
+    public boolean withdraw(Double amount) {
+
+        if (amount > 0 && amount <= this.balance && (this.dailyWithdrawnAmount + amount) <= this.dailyWithdrawalLimit) {
+            this.balance -= amount;
+            this.dailyWithdrawnAmount += amount;
+            this.history.add("Withdrawal: " + amount);
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/tdd/TestBankAccount.java
+++ src/test/java/tdd/TestBankAccount.java
@@ -3,8 +3,7 @@ package tdd;
 import com.tpe.tdd.BankAccount;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestBankAccount {
 
@@ -29,5 +28,81 @@ public class TestBankAccount {
         assertEquals("12345", bankAccount.getAccountNumber());
         assertEquals(0.0, bankAccount.getBalance());
         assertEquals(1000.0, bankAccount.getDailyWithdrawalLimit());
+    }
+
+    @Test
+    void testDeposit() {
+//        If we had BDD, this would be our "given" part.
+        BankAccount bankAccount = new BankAccount("12345", 0.0, 1000.0);
+        int initialHistorySize = bankAccount.getHistory().size();
+
+//        Then this would be "when" part.
+        bankAccount.deposit(200.0);
+
+//        "Then" part.
+        assertEquals(200.0, bankAccount.getBalance());
+        assertEquals(initialHistorySize + 1, bankAccount.getHistory().size());
+    }
+
+    @Test
+    void testDepositWithNonPositiveAmount() {
+        BankAccount bankAccount = new BankAccount("12345", 0.0, 1000.0);
+        int initialHistorySize = bankAccount.getHistory().size();
+
+//        bankAccount.deposit(-200.0);
+
+        assertThrows(RuntimeException.class, () -> bankAccount.deposit(-200.0));
+        assertEquals(0.0, bankAccount.getBalance());
+        assertEquals(initialHistorySize, bankAccount.getHistory().size());
+    }
+
+    @Test
+    void testWithdraw() {
+        BankAccount bankAccount = new BankAccount("12345", 500.0, 1000.0);
+        int initialHistorySize = bankAccount.getHistory().size();
+        assertTrue(bankAccount.withdraw(200.0));
+        assertEquals(300.0, bankAccount.getBalance());
+        assertEquals(initialHistorySize + 1, bankAccount.getHistory().size());
+    }
+
+    @Test
+    void testWithdrawWhenInsufficientBalance() {
+        BankAccount bankAccount = new BankAccount("12345", 500.0, 1000.0);
+        int initialHistorySize = bankAccount.getHistory().size();
+        assertFalse(bankAccount.withdraw(600.0));
+        assertEquals(500.0, bankAccount.getBalance());
+        assertEquals(initialHistorySize, bankAccount.getHistory().size());
+    }
+
+    @Test
+    void testWithdrawWhenAmountOverDailyLimit() {
+        BankAccount bankAccount = new BankAccount("12345", 1500.0, 1000.0);
+        int initialHistorySize = bankAccount.getHistory().size();
+
+        assertTrue(bankAccount.withdraw(1000.0));
+        assertFalse(bankAccount.withdraw(100.0));   // Withdraw not available
+        assertEquals(500.0, bankAccount.getBalance());
+        assertEquals(initialHistorySize + 1, bankAccount.getHistory().size());
+    }
+
+    @Test
+    void testWithdrawWithOnePositiveAmount() {
+        BankAccount bankAccount = new BankAccount("12345", 500.0, 1000.0);
+        int initialHistorySize = bankAccount.getHistory().size();
+        assertFalse(bankAccount.withdraw(-200.0));
+        assertEquals(500.0, bankAccount.getBalance());
+        assertEquals(initialHistorySize, bankAccount.getHistory().size());
+    }
+
+    @Test
+    void testGetAccountTransactionHistory() {
+        BankAccount bankAccount = new BankAccount("12345", 500.0, 1000.0);
+        int initialHistorySize = bankAccount.getHistory().size();
+
+        bankAccount.withdraw(200.0);    // Size + 1
+        bankAccount.deposit(500.0); // Size + 1
+
+        assertEquals(initialHistorySize + 2, bankAccount.getHistory().size());
+        assertTrue(bankAccount.getHistory().contains("Withdrawal: " + 200.0));
     }
 }
